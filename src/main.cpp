3f63fdf5d71c4d4e449481b73d5d62a02b966123
scartezini <escc.carvalho@gmail.com>
2020-08-10 22:23:05 -0300

    Change the main to run the tests
    - Now the main function will receive the .nes file with a ROM test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 #include "cpu6502.h"
+#include "utils.h"
 
-int main() {
+int main( int argc, char *argv[ ] ) {
 	Cpu6502 cpu;
+
+	Utils::load_ROM(cpu.bus, argv[1]);
 
 	for (int i = 0; i < 4; i++) cpu.clock();
 }
